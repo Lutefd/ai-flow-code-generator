4ec85c469f6d0a602cf39809a80035066060924e
luis dourado <lfaddourado@gmail.com>
2023-07-19 13:59:19 -0300

    feat: add parseable stream and role based parsing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
-    import { useChat} from "ai/svelte"
-    const { messages, handleSubmit, input} = useChat({
-        api: "/chat"
-    })
+	import { useChat } from 'ai/svelte';
+	import { m2 } from '../store/stores';
+	import AiMessage from '../components/AiMessage.svelte';
+	import { goto } from '$app/navigation';
+	const { messages, handleSubmit, input } = useChat({
+		api: '/chat'
+	});
+	$: exportConversation = async () => {
+		$m2 = $messages;
+		goto('/DS');
+	};
 </script>
 
 <ul>
-    {#each $messages as message}
-        <li>{message.role} says: {message.content}</li>
-    {/each}
+	{#each $messages as message}
+		{#if message.role === 'user'}
+			<li>you said: {message.content}</li>
+		{/if}
+		{#if message.role === 'assistant'}
+			<li>bot says: <AiMessage message={message.content} /></li>
+		{/if}
+	{/each}
 </ul>
-
 <form on:submit={handleSubmit}>
-    <input bind:value={$input} />
-    <button type="submit">Send</button>
+	<input bind:value={$input} />
+	<button type="submit">Send</button>
 </form>
+<button on:click={exportConversation}>Export this conversation</button>
