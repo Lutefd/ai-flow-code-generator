4a1b620c2c3a5831e10e83469f2f0df8c6037c53
luis dourado <lfaddourado@gmail.com>
2023-07-19 18:24:12 -0300

    feat: add basic styling and auto-scroll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,27 +3,73 @@
 	import { m2 } from '../store/stores';
 	import AiMessage from '../components/AiMessage.svelte';
 	import { goto } from '$app/navigation';
+	import { afterUpdate } from 'svelte';
+
 	const { messages, handleSubmit, input } = useChat({
-		api: '/chat'
+		api: '/chat',
+		onResponse: (response) => {
+			console.log(response);
+		},
+		body: {
+			department: 'sales',
+			product: 'shoes'
+		}
 	});
 	$: exportConversation = async () => {
 		$m2 = $messages;
 		goto('/DS');
 	};
+	let canvas: HTMLElement;
+	const scrollToBottom = async (node: HTMLElement) => {
+		node.scroll({ top: node.scrollHeight, behavior: 'smooth' });
+	};
+	afterUpdate(() => {
+		console.log('afterUpdate');
+		if (messages) scrollToBottom(canvas);
+	});
+	$: if (messages && canvas) {
+		console.log('tick');
+		scrollToBottom(canvas);
+	}
 </script>
 
-<ul>
-	{#each $messages as message}
-		{#if message.role === 'user'}
-			<li>you said: {message.content}</li>
-		{/if}
-		{#if message.role === 'assistant'}
-			<li>bot says: <AiMessage message={message.content} /></li>
-		{/if}
-	{/each}
-</ul>
-<form on:submit={handleSubmit}>
-	<input bind:value={$input} />
-	<button type="submit">Send</button>
-</form>
-<button on:click={exportConversation}>Export this conversation</button>
+<div class="grid max-h-[48rem] overflow-y-scroll w-screen place-items-center" bind:this={canvas}>
+	<div class="w-[32rem]">
+		<ul>
+			{#each $messages as message}
+				{#if message.role === 'user'}
+					<li>
+						<div class="chat chat-start">
+							<div class="chat-bubble">{message.content}</div>
+						</div>
+					</li>
+				{/if}
+				{#if message.role === 'assistant'}
+					<li>
+						<div class="chat chat-end max-w-xl">
+							<div class="chat-bubble"><AiMessage message={message.content} /></div>
+						</div>
+					</li>{/if}
+			{/each}
+		</ul>
+	</div>
+</div>
+<div class="w-screen flex justify-center">
+
+	<form on:submit={handleSubmit} class="absolute bottom-5">
+		<div class="flex gap-4">
+			<input bind:value={$input} class="input input-bordered input-secondary w-full max-w-xs" />
+			<button type="submit" class="btn-secondary w-32 rounded-lg">Send</button>
+		</div>
+	</form>
+</div>
+
+<style>
+	::-webkit-scrollbar {
+		width: 0.4rem;
+	}
+	::-webkit-scrollbar-thumb {
+		background-color: #888;
+		border-radius: 1rem;
+	}
+</style>
--- src/routes/chat/+server.ts
+++ src/routes/chat/+server.ts
@@ -10,11 +10,15 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 export const POST: RequestHandler = async ({ request }) => {
-	const { messages } = await request.json();
+	const { messages, department, product } = await request.json();
+	console.log(messages, department, product);
 	const response = await openai.createChatCompletion({
 		model: 'gpt-3.5-turbo',
 		stream: true,
-		messages
+		messages: messages.map((message: any) => ({
+			content: message.content,
+			role: message.role
+		}))
 	});
 
 	const stream = OpenAIStream(response);
