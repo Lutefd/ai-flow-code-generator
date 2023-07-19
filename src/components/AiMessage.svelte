d3b247169528f2fadce8c0cfa6bc3df76fa3e1b3
luis dourado <lfaddourado@gmail.com>
2023-07-19 13:56:15 -0300

    feat: create custom ai component for parsing

---
+++ src/components/AiMessage.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { marked } from 'marked';
+	marked.use({ mangle: false, headerIds: false });
+	export let message = '';
+	let html = '';
+	$: html = marked.parse(message);
+</script>
+
+{@html html}
