8abee9ad0287cf6176e015ebb0d070f3a241126f
luis dourado <lfaddourado@gmail.com>
2023-07-21 10:43:12 -0300

    feat: adapt to the edge

--- src/routes/chat/+server.ts
+++ src/routes/chat/+server.ts
@@ -11,7 +11,6 @@ const openai = new OpenAIApi(config);
 
 export const POST: RequestHandler = async ({ request }) => {
 	const { messages, department, product } = await request.json();
-	console.log(messages, department, product);
 	const response = await openai.createChatCompletion({
 		model: 'gpt-3.5-turbo',
 		stream: true,
