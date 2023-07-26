a9f865341e6d2254b9e63b188eff401eae1a6699
luis dourado <lfaddourado@gmail.com>
2023-07-26 19:26:14 -0300

    feat: differentiate endpoints based on stage of the conversation

--- src/routes/chat/+server.ts
+++ src/routes/chat/+server.ts
@@ -10,23 +10,14 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 export const POST: RequestHandler = async ({ request }) => {
-	const { messages, department } = await request.json();
-	console.log(department);
+	const { messages, department, product, attributes } = await request.json();
 	const response = await openai.createChatCompletion({
 		model: 'gpt-3.5-turbo-16k',
 		stream: true,
 		messages: messages.map((message: any) => ({
-			content: `Olá, preciso de uma chamada de api em javascript para criar um access-token, onde preciso passar esse app ${department} com esse Owner: sensedia@teste.com.br e status "ACTIVE" esse é o swagger {  
-    "app": {
-    "clientId": "7bc8f669-6fe5-4a38-9b04-1afa7b4991e1",
-    "id": 2258,
-    "name": "App Teste 16 - 12 - 22",
-    "status": "APPROVED"
-  },
-  "owner": "OwnerTeste",
-  "status": "ACTIVE"
-}
-`,
+			content: `Baseado nos seguintes parametros department:${department},productType:${product}, attributes:${JSON.stringify(
+				attributes
+			)} e em nosso histórico de chat,  ${message.content}`,
 			role: message.role
 		}))
 	});
---
+++ src/routes/initialChat/+server.ts
@@ -0,0 +1,67 @@
+import type { RequestHandler } from './$types';
+import { Configuration, OpenAIApi } from 'openai-edge';
+import { OPENAI_KEY } from '$env/static/private';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+const config = new Configuration({
+	apiKey: OPENAI_KEY
+});
+
+const openai = new OpenAIApi(config);
+
+export const POST: RequestHandler = async ({ request }) => {
+	const { messages, department, product, attributes } = await request.json();
+	const response = await openai.createChatCompletion({
+		model: 'gpt-3.5-turbo-16k',
+		stream: true,
+		messages: messages.map((message: any) => ({
+			content: `Olá, preciso de uma chamada de api em para criar um produto em uma api, onde preciso passar esse departamento: ${
+				department.name
+			} e esse productType: ${product.name} e esses atributos ${JSON.stringify(
+				attributes
+			)}.Nesse modelo a seguir, as informações não passadas previamente apenas preencha com "placeholder":
+			{
+  "sku": "string",
+  "productGroup": "string",
+  "department": "string",
+  "productType": "string",
+  "brand": "string",
+  "name": "string",
+  "description": "string",
+  "color": "string",
+  "flavor": "string",
+  "gender": "Homem",
+  "manufacturerCode": "string",
+  "size": "string",
+  "eanIsbn": "string",
+  "height": 0,
+  "width": 0,
+  "depth": 0,
+  "weight": 0,
+  "video": "string",
+  "images": [
+    {
+      "url": "string"
+    }
+  ],
+  "attributes": [
+    {
+      "name": "string",
+      "values": [
+        "string"
+      ]
+    }
+  ]
+}
+
+me dê um modelo de código
+
+
+`,
+			role: message.role
+		}))
+	});
+
+	const stream = OpenAIStream(response);
+	return new StreamingTextResponse(stream);
+};
