2b86378255999ab0390d6f6acb3b8fe073fefaf3
luis dourado <lfaddourado@gmail.com>
2023-07-27 12:38:39 -0300

    feat: add language and copy bar to code blocks

--- src/components/AiMessage.svelte
+++ src/components/AiMessage.svelte
@@ -2,13 +2,26 @@
 	import { Marked } from 'marked';
 	import hljs from 'highlight.js';
 	import { markedHighlight } from 'marked-highlight';
+	let id = 0;
 
 	const marked = new Marked(
 		markedHighlight({
-			langPrefix: 'hljs language-',
+			langPrefix: '!p-0 my-4 !flex relative hljs language-',
 			highlight(code, lang) {
 				const language = hljs.getLanguage(lang) ? lang : 'shell';
-				return hljs.highlight(code, { language }).value;
+				const highlightedCode = hljs.highlight(code, { language }).value;
+				const result = `
+        <div class="code-block w-full flex flex-col">
+            <div class="title-bar flex justify-between items-center bg-gray-900 absolute top-0 w-full px-2 py-1">
+                <span class="language-label">${language}</span>
+                <button class="btn-primary bg-gray-200 text-gray-900 h-6 px-2 rounded-md" data-clipboard-target="#code-block-${id}">Copy</button>
+            </div>
+            <div class="code-container overflow-x-auto px-4 flex pt-12 ">
+                <pre id="code-block-${id}" class="hljs language-${lang} code-overflow"><code>${highlightedCode}</code></pre>
+            </div>
+        </div>`;
+				id += 1;
+				return result;
 			}
 		})
 	);
@@ -16,7 +29,23 @@
 
 	export let message = '';
 	let html = '';
-	$: html = marked.parse(message);
+	$: {
+		html = marked.parse(message);
+		// Use event delegation to handle copy button clicks
+		document.body.addEventListener('click', (event) => {
+			const target = event.target as HTMLElement; // cast event.target to HTMLElement
+			if (target.matches('.btn-primary')) {
+				const copyTargetId = target.getAttribute('data-clipboard-target');
+				if (copyTargetId) {
+					const codeBlock = document.querySelector(copyTargetId) as HTMLElement;
+					if (codeBlock) {
+						const copyText = codeBlock.innerText;
+						navigator.clipboard.writeText(copyText);
+					}
+				}
+			}
+		});
+	}
 </script>
 
 {@html html}
@@ -29,19 +58,32 @@
 		display: block;
 		width: 100%;
 	}
-
+	pre code.hljs {
+		padding: 0 !important;
+	}
+	code {
+		padding: 0 !important;
+	}
 	textarea {
 		font-family: monospace, Roboto;
 		height: 25%;
 		border: none;
 		margin: 0;
-		padding: 1rem;
+		padding-inline: 1rem;
 	}
 
 	.preview {
 		height: 75%;
-		padding: 2rem;
+		padding-inline: 2rem;
 		border-top: solid 2px #888;
+	}
+	pre {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
+	code {
+		padding: 0;
 	}
 	pre code {
 		font-size: 0.75rem;
@@ -59,5 +101,21 @@
 	:global(blockquote) {
 		border-left-color: #ccd1d8;
 		border-right-color: #aab2bc;
+	}
+	.title-bar {
+		font-family: monospace;
+		font-size: 0.9em;
+	}
+
+	.language-label {
+		color: #333;
+	}
+	:global(::-webkit-scrollbar) {
+		width: 0.4rem !important;
+		height: 0.4rem !important;
+	}
+	:global(::-webkit-scrollbar-thumb) {
+		background-color: #fff !important;
+		border-radius: 1rem !important;
 	}
 </style>
