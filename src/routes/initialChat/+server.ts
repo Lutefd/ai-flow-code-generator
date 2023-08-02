8c2f699c956d9c46ca55c4911da7bfd4ce2a72a2
luis dourado <lfaddourado@gmail.com>
2023-08-02 13:32:47 -0300

    fix: crash on big attributes

--- src/routes/initialChat/+server.ts
+++ src/routes/initialChat/+server.ts
@@ -1,4 +1,5 @@
 import type { RequestHandler } from './$types';
+import type { Message } from '../../types/Interfaces';
 import { Configuration, OpenAIApi } from 'openai-edge';
 import { OPENAI_KEY } from '$env/static/private';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
@@ -12,15 +13,29 @@ const openai = new OpenAIApi(config);
 export const POST: RequestHandler = async ({ request }) => {
 	const { messages, department, product, attributes } = await request.json();
 	console.log(attributes);
+	if (attributes.length < 6) {
+		for (const attribute of attributes) {
+			if (attribute.values && JSON.parse(attribute.values).length > 8) {
+				attribute.values = JSON.stringify(JSON.parse(attribute.values).slice(0, 5));
+			}
+		}
+	} else {
+		for (const attribute of attributes) {
+			if (attribute.values && JSON.parse(attribute.values).length > 8) {
+				attribute.values = JSON.stringify(JSON.parse(attribute.values).slice(0, 3));
+			}
+		}
+	}
+	console.log('attributes2', attributes);
 	const response = await openai.createChatCompletion({
 		model: 'gpt-3.5-turbo-16k-0613',
 		stream: true,
-		messages: messages.map((message: any) => ({
-			content: `Olá, preciso de uma chamada de api em para criar um produto em uma api, onde preciso passar esse departamento: ${
+		messages: messages.map((message: Message) => ({
+			content: `Olá, preciso de uma chamada de api em para criar um produto, esse é o endpoint: "https://api-sandbox.netshoes.com.br/api/v2/products/", onde preciso passar esse departamento: ${
 				department.name
 			} e esse productType: ${product.name} e esses atributos ${JSON.stringify(
 				attributes
-			)}.Nesse modelo a seguir, as informações não passadas previamente apenas preencha com "placeholder", caso os attributes possuam values com "code e values" apenas preencha com os objetos como strings, caso não possua apenas preencha com "placeholder":
+			)}.Nesse modelo a seguir, as informações não passadas previamente apenas preencha com "placeholder", caso os attributes possuam values com "code e values" apenas preencha com os objetos que possuem "code e values" e caso não possuam apenas preencha com "placeholder":
 			{
   "sku": "string",
   "productGroup": "string",
@@ -62,7 +77,7 @@ ${message.content}
 			role: message.role
 		}))
 	});
-
+	console.log(response);
 	const stream = OpenAIStream(response);
 	return new StreamingTextResponse(stream);
 };
